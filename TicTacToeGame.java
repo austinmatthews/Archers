e3fd5b8c85f347ab1bd44cd7dda08c6b1fea4275
Matthew Vollkommer <mvollk@uga.edu>
2014-04-06 20:25:02 -0400

    GamePlays
    Need to add undo functionality.
    Need to prevent writing over previous moves.
    Need to add winner notification and option to
    replay or return to menu

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -20,15 +20,18 @@ public class TicTacToeGame implements MouseListener
 	int theFlag = 2;
 	int theY;
 	int theX;
-
+	boolean won;
 	//a game board is a 3X3 2D array of X's and O's
-	char[][] charArray = new char[2][2];
+	char[][] charArray = new char[3][3];
+	
 
 	//constructor
 	public TicTacToeGame()
 	{
+		
+		won = false;
 		//initialize the JFrame
-		theJFrame = new JFrame("Tic Tac Toe");
+		theJFrame = new JFrame("player O");
 		theJFrame.setSize(1000, 700);
 		theJFrame.setVisible(true);
 		//add listeners
@@ -38,7 +41,7 @@ public class TicTacToeGame implements MouseListener
 		for (int counter = 0; counter < 3; counter++){
 			for(int counterTwo = 0; counterTwo < 3; counterTwo++){
 			//make the char array all a's
-			charArray[counter][counterTwo] = 'a';
+			charArray[counter][counterTwo] = '_';
 			}
 		
 		}
@@ -60,6 +63,7 @@ public class TicTacToeGame implements MouseListener
 		//exit on close
 		theJFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 		this.drawTheBoard();
+		
 
 		theJFrame.setVisible(true);
 	}
@@ -71,6 +75,7 @@ public class TicTacToeGame implements MouseListener
 
 	}
 
+	//draw the gameboard, 9 boxes are made
 	public void drawTheBoard(){
 		Graphics moreGraphics = theJFrame.getGraphics();
 		moreGraphics.drawLine(200, 0, 200, 600);
@@ -88,9 +93,7 @@ public class TicTacToeGame implements MouseListener
 		//draw the board
 		Graphics moreGraphics = theJFrame.getGraphics();
 
-
-
-
+		//get the location the mouse was pressed
 		int x = e.getX();
 		int y = e.getY();
 
@@ -101,7 +104,14 @@ public class TicTacToeGame implements MouseListener
 
 		//subtract one from the flag on each pass through
 		theFlag -= 1;
-
+		
+		//tell the player it is their turn
+		if(theFlag == 1){
+			theJFrame.setTitle("player X");
+		}else{
+			theJFrame.setTitle("player O");
+		}
+		
 		//if the flag is a 1, draw a circle
 		if (theFlag == 1)
 		{
@@ -119,45 +129,87 @@ public class TicTacToeGame implements MouseListener
 			moreGraphics.setColor(Color.BLACK);
 			theGraphics2D = (Graphics2D)moreGraphics;
 			theGraphics2D.setStroke(new BasicStroke(10.0F));
-
 			moreGraphics.drawOval(theX + 10,theY+ 10, 160, 160);
+
 		}
 
 		//if theflag is a 0, draw an X
 		if (theFlag == 0)
 		{
-			if ((x < 200) && (y < 200)) { theX = 0; theY = 0; charArray[0][0] = 'O'; }
-			if ((x > 200) && (x < 400) && (y < 200)) { theX = 200; theY= 0; charArray[0][1] = 'O'; }
-			if ((x > 400) && (x < 600) && (y < 200)) { theX = 400; theY= 0; charArray[0][2] = 'O'; }
-			if ((x < 200) && (y > 200) && (y < 400)) { theX = 0; theY= 200; charArray[1][0] = 'O'; }
-			if ((x > 200) && (x < 400) && (y > 200) && (y < 400)) { theX = 200; theY= 200; charArray[1][1] = 'O'; }
-			if ((x > 400) && (x < 600) && (y > 200) && (y < 400)) { theX = 400; theY= 200; charArray[1][2] = 'O'; }
-			if ((x < 200) && (y > 400) && (y < 600)) { theX = 0; theY= 400; charArray[2][0] = 'O'; }
-			if ((x > 200) && (x < 400) && (y > 400) && (y < 600)) { theX = 200; theY= 400; charArray[2][1] = 'O'; }
-			if ((x > 400) && (x < 600) && (y > 400) && (y < 600)) { theX = 400; theY= 400; charArray[2][2] = 'O'; }
+			if ((x < 200) && (y < 200)) { theX = 0; theY = 0; charArray[0][0] = 'X'; }
+			if ((x > 200) && (x < 400) && (y < 200)) { theX = 200; theY= 0; charArray[0][1] = 'X'; }
+			if ((x > 400) && (x < 600) && (y < 200)) { theX = 400; theY= 0; charArray[0][2] = 'X'; }
+			if ((x < 200) && (y > 200) && (y < 400)) { theX = 0; theY= 200; charArray[1][0] = 'X'; }
+			if ((x > 200) && (x < 400) && (y > 200) && (y < 400)) { theX = 200; theY= 200; charArray[1][1] = 'X'; }
+			if ((x > 400) && (x < 600) && (y > 200) && (y < 400)) { theX = 400; theY= 200; charArray[1][2] = 'X'; }
+			if ((x < 200) && (y > 400) && (y < 600)) { theX = 0; theY= 400; charArray[2][0] = 'X'; }
+			if ((x > 200) && (x < 400) && (y > 400) && (y < 600)) { theX = 200; theY= 400; charArray[2][1] = 'X'; }
+			if ((x > 400) && (x < 600) && (y > 400) && (y < 600)) { theX = 400; theY= 400; charArray[2][2] = 'X'; }
 			
 			
 			//drawing an X
 			theGraphics2D = (Graphics2D)moreGraphics;
 			theGraphics2D.setStroke(new BasicStroke(10.0F));
 			theGraphics2D.setColor(Color.RED);
-			theGraphics2D.drawLine(theX + 10,theY+ 13,theX+ 170,theY+ 160);
+			theGraphics2D.drawLine(theX + 10,theY+ 10,theX+ 170,theY+ 160);
 			theGraphics2D.drawLine(theX + 170,theY+ 10,theX+ 10,theY+ 170);
 
 			//cycle back through the flag, setting the flag to 2
 			theFlag = 2;
+
 		}
 
 		//check to see if the game has ended
-		this.gameOver();
+		this.winner();
+		
+		//print array for testing this.printArray();
 	}
 
+	
+	//is the there a winner?
+	public void winner(){
 
-	public void gameOver(){
+		//is there a winner on a row or a column?
+		for (int counter = 0; counter< 3; counter++){
+			
+			if((charArray[counter][0] == charArray[counter][1]) && (charArray[counter][0] == charArray[counter][2]) && charArray[counter][0] != '_') {
+				won = true;
+			}
+			
+			if((charArray[0][counter] == charArray[1][counter]) && (charArray[1][counter] == charArray[2][counter]) && charArray[0][counter] != '_'){
+				won = true;
+			}
+		}
+		
+		//is there a diagonal winner?
 
+		if(charArray[0][0] == charArray[1][1] && charArray [0][0] == charArray[2][2] && charArray[1][1] != '_'){
+				won = true;
+		}
+		
+		if(charArray[0][2] == charArray[1][1] && charArray[0][2] == charArray[2][0] && charArray[1][1] != '_'){
+			won = true;
+		}
+		
+		if(won == true){
 
+		}
 	}
 
+	//print the 2D char array for testing purposes
+	
+	/* public void printArray(){
+		
+		for(int counter = 0; counter < 3; counter++){
+			
+			for(int counterTwo = 0; counterTwo < 3; counterTwo++){
+				System.out.print(charArray[counter][counterTwo] + ",");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+	*/
 
 
 	// main method for testing
