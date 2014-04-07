fb6ed2a8abe8a00064a343c7fe6ffa238780a594
austm14 <austm14@gmail.com>
2014-04-06 23:49:51 -0400

    4/6

--- MainMenu.java
+++ MainMenu.java
@@ -1,100 +1,27 @@
 
-		import java.awt.*;
-
-		import java.awt.event.ActionEvent;
-
-		import java.awt.event.ActionListener;
-
-		import javax.swing.*;
+import java.awt.*;
+import javax.swing.*;
 
 
+//contains the main class
+public class MainMenu extends JFrame {
+	
+	//creates the frame and the main panel (mainMenu) that we are going to be using for cardLayout to switch other panels on
+	public static MainMenu frame = new MainMenu();
+	public static JPanel mainMenu = new JPanel();
+	public static CardLayout cardLayout = new CardLayout();
 
-		public class MainMenu {
-
-			public static MainMenu frame = new MainMenu();
-
-			public static JPanel mainMenu = new JPanel();
-
-			public static CardLayout cardLayout = new CardLayout();
-
-
-/*
-			public MainMenu() {
-
-				setTitle("M&M Arcade");
-
-				setSize(1000, 1000);
-
-				mainMenu.setLayout(cardLayout);
-
-				gamePanel = new JPanel();
-
-				jp2 = new JPanel();
-
-				jl1 = new JLabel("Card 1");
-
-				jl2 = new JLabel("Card 2");
-
-				jp1.add(jl1);
-
-				jp2.add(jl2);
-
-				mainMenu.add(jp1, "1");
-
-				mainMenu.add(jp2, "2");
-
-				btn1 = new JButton("Show Card 1");
-
-				btn1.addActionListener(new ActionListener() {
-
-
-
-					public void actionPerformed(ActionEvent e) {
-
-						cardLayout.show(mainMenu, "1");
-
-					}
-
-				});
-
-				btn2 = new JButton("Show Card 2");
-
-				btn2.addActionListener(new ActionListener() {
-
-
-
-					public void actionPerformed(ActionEvent e) {
-
-						cardLayout.show(mainMenu, "2");
-
-					}
-
-				});
-
-				buttonPanel.add(btn1);
-
-				buttonPanel.add(btn2);
-
-				add(mainMenu, BorderLayout.NORTH);
-
-				add(buttonPanel, BorderLayout.SOUTH);
-
-			}
-*/
-
-
-			public static void main(String[] args) {
-
-						frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-						frame.setVisible(true);
-
-					}
-
-				});
-
-			}
+	public static void main(String[] args) {
+		
+		//creates the frame and sets 
+		mainMenu.setLayout(cardLayout);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("M&M Arcade");
+		frame.setSize(1000, 1000);
+		frame.setVisible(true);
+		mainMenu.setBounds(0, 0, 1000, 1000);
+		mainMenu.setBackground(Color.black);
+		frame.getContentPane().add(mainMenu);
 
 	}
-
-
+}
