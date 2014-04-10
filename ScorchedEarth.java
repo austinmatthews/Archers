a028d916cb1a22e6514a37e91f5f68563f55e642
austm14 <austm14@gmail.com>
2014-04-10 15:34:40 -0400

    4/10 first commit

--- ScorchedEarth.java
+++ ScorchedEarth.java
@@ -16,6 +16,9 @@ import java.awt.event.MouseMotionAdapter;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Queue;
+import java.util.Stack;
 
 import javax.imageio.ImageIO;
 import javax.swing.Box;
@@ -43,6 +46,7 @@ import javax.swing.event.MouseInputAdapter;
 
 public class ScorchedEarth {
 
+
 	JPanel scorchedMenu = new JPanel();
 	JPanel scorchedGame = new JPanel();
 	JPanel scorchedInfo = new JPanel();
@@ -50,15 +54,12 @@ public class ScorchedEarth {
 	JPanel filler2 = new JPanel();
 	JPanel scorchedArena = new JPanel();
 	JPanel scorchedTurns = new JPanel();
-	JSlider p1PowerSlider = new JSlider();
-	JSlider p1AngleSlider = new JSlider();
-	JSlider p2PowerSlider = new JSlider();
-	JSlider p2AngleSlider = new JSlider();
-	JButton next = new JButton("Next");
-	JLabel p1Power = new JLabel();
-	JLabel p2Power = new JLabel();
-	JLabel p1Angle = new JLabel();
-	JLabel p2Angle = new JLabel();
+	JSlider powerSlider = new JSlider();
+	JSlider angleSlider = new JSlider();
+	JLabel next = new JLabel("Next");
+	JLabel power = new JLabel();
+	JLabel angle = new JLabel();
+	JLabel turnIndicator = new JLabel();
 	JLabel wallLabel;
 	JLabel p1ArcherLabel;
 	JLabel p2ArcherLabel;
@@ -67,9 +68,12 @@ public class ScorchedEarth {
 	int archerY = 338;
 	int archer1X;
 	int archer2X;
+	int click = 0;
+	int turn = 1;
 
 	public ScorchedEarth(){
 		initializeGame();
+		playGame();
 	}
 
 	public void initializeGame() {
@@ -78,10 +82,10 @@ public class ScorchedEarth {
 		scorchedGame.setBounds(0, 0, 1000, 850);
 		scorchedInfo.setMaximumSize(new Dimension(1000, 140));
 		filler.setMaximumSize(new Dimension(1000, 4));
-		scorchedArena.setMaximumSize(new Dimension(1000, 450));
+		scorchedArena.setMaximumSize(new Dimension(1000, 500));
 		scorchedArena.setLayout(null);
 		filler2.setMaximumSize(new Dimension(1000, 4));
-		scorchedTurns.setMaximumSize(new Dimension(1000, 260));
+		scorchedTurns.setMaximumSize(new Dimension(1000, 210));
 
 
 		//sets the background colors of the game panels
@@ -110,89 +114,60 @@ public class ScorchedEarth {
 
 	public void setTurns() {
 
-		p1PowerSlider = new JSlider(JSlider.HORIZONTAL, 0, 100, 0);
-		p1PowerSlider.setBackground(Color.BLACK);
-		p1PowerSlider.setForeground(Color.WHITE);
-		p1PowerSlider.setMajorTickSpacing(10);
-		p1PowerSlider.setPaintTicks(true);
-		p1PowerSlider.setPaintLabels(true);
-		p1PowerSlider.setAlignmentX(Component.LEFT_ALIGNMENT);
-		p1PowerListener powerListener = new p1PowerListener();
-		p1PowerSlider.addChangeListener(powerListener);
-		p1PowerSlider.setPreferredSize(new Dimension (400, 40));
+		powerSlider = new JSlider(JSlider.HORIZONTAL, 0, 100, 0);
+		powerSlider.setMajorTickSpacing(10);
+		powerSlider.setPaintTicks(true);
+		powerSlider.setPaintLabels(true);
+		powerListener powerListener = new powerListener();
+		powerSlider.addChangeListener(powerListener);
+		powerSlider.setPreferredSize(new Dimension (400, 40));
 
-		p2PowerSlider = new JSlider(JSlider.HORIZONTAL, 0, 100, 0);
-		p2PowerSlider.setBackground(Color.BLACK);
-		p2PowerSlider.setForeground(Color.WHITE);
-		p2PowerSlider.setMajorTickSpacing(10);
-		p2PowerSlider.setPaintTicks(true);
-		p2PowerSlider.setPaintLabels(true);
-		p2PowerSlider.setAlignmentX(Component.RIGHT_ALIGNMENT);
-		p2PowerListener p2PowerListener = new p2PowerListener();
-		p2PowerSlider.addChangeListener(p2PowerListener);
-		p2PowerSlider.setPreferredSize(new Dimension (400, 40));
+		angleSlider = new JSlider(JSlider.HORIZONTAL, 0, 90, 0);
+		angleSlider.setMajorTickSpacing(5);
+		angleSlider.setPaintTicks(true);
+		angleSlider.setPaintLabels(true);
+		angleListener p1AngleListener = new angleListener();
+		angleSlider.addChangeListener(p1AngleListener);
+		angleSlider.setPreferredSize(new Dimension (400, 40));
 
-		p1AngleSlider = new JSlider(JSlider.HORIZONTAL, 0, 90, 0);
-		p1AngleSlider.setBackground(Color.BLACK);
-		p1AngleSlider.setForeground(Color.WHITE);
-		p1AngleSlider.setMajorTickSpacing(5);
-		p1AngleSlider.setPaintTicks(true);
-		p1AngleSlider.setPaintLabels(true);
-		p1AngleSlider.setAlignmentX(Component.LEFT_ALIGNMENT);
-		p1AngleListener p1AngleListener = new p1AngleListener();
-		p1AngleSlider.addChangeListener(p1AngleListener);
-		p1AngleSlider.setPreferredSize(new Dimension (400, 40));
+		turnIndicator = new JLabel("P1 - Turn " + turn);
+		turnIndicator.setFont(new Font("Andalus", Font.BOLD, 40));
+		turnIndicator.setForeground(Color.white);
 
-		p2AngleSlider = new JSlider(JSlider.HORIZONTAL, 0, 90, 0);
-		p2AngleSlider.setMajorTickSpacing(5);
-		p2AngleSlider.setPaintTicks(true);
-		p2AngleSlider.setPaintLabels(true);
-		p2AngleSlider.setAlignmentX(Component.RIGHT_ALIGNMENT);
-		p2AngleListener p2AngleListener = new p2AngleListener();
-		p2AngleSlider.addChangeListener(p2AngleListener);
-		p2AngleSlider.setPreferredSize(new Dimension (400, 40));
+		next.setFont(new Font("Andalus", Font.BOLD, 25));
+		next.setForeground(Color.white);
 
-		p1Power = new JLabel("P1 Power");
-		p1Power.setFont(new Font("Andalus", Font.BOLD, 40));
-		p1Power.setForeground(Color.white);
+		power = new JLabel("Power");
+		power.setFont(new Font("Andalus", Font.BOLD, 40));
+		power.setForeground(Color.white);
 
-		p2Power = new JLabel("P2 Power");
-		p2Power.setFont(new Font("Andalus", Font.BOLD, 40));
-		p2Power.setForeground(Color.white);
+		angle = new JLabel("Angle");
+		angle.setFont(new Font("Andalus", Font.BOLD, 40));
+		angle.setForeground(Color.white);
 
-		p1Angle = new JLabel("P1 Angle");
-		p1Angle.setFont(new Font("Andalus", Font.BOLD, 40));
-		p1Angle.setForeground(Color.white);
-
-		p2Angle = new JLabel("P2 Angle");
-		p2Angle.setFont(new Font("Andalus", Font.BOLD, 40));
-		p2Angle.setForeground(Color.white);
-
-		scorchedTurns.add(p1Power);
-		scorchedTurns.add(Box.createRigidArea(new Dimension(290,5)));
-		scorchedTurns.add(p2Power);
-		scorchedTurns.add(p1PowerSlider);
-		scorchedTurns.add(Box.createRigidArea(new Dimension(60,5)));
-		scorchedTurns.add(p2PowerSlider);
-		scorchedTurns.add(p1Angle);
-		scorchedTurns.add(Box.createRigidArea(new Dimension(300,5)));
-		scorchedTurns.add(p2Angle);
-		scorchedTurns.add(p1AngleSlider);
+		scorchedTurns.add(Box.createRigidArea(new Dimension(350,5)));
+		scorchedTurns.add(turnIndicator);
+		scorchedTurns.add(Box.createRigidArea(new Dimension(350,5)));
+		scorchedTurns.add(power);
+		scorchedTurns.add(Box.createRigidArea(new Dimension(375,5)));
+		scorchedTurns.add(angle);
+		scorchedTurns.add(powerSlider);
 		scorchedTurns.add(next);
-		scorchedTurns.add(p2AngleSlider);
+		scorchedTurns.add(angleSlider);
+		MouseListener next = new MouseListener();
 	}
 
 	public void setArena() {
 		archersX();
 		importWall();
 		scorchedArena.add(wallLabel);
-		wallLabel.setBounds(450, 200, 100, 275);
+		wallLabel.setBounds(450, 200, 100, 300);
 		importArchers();
 		scorchedArena.add(p1ArcherLabel);
-		p1ArcherLabel.setBounds(archer1X, 338, 71, 100);
+		p1ArcherLabel.setBounds(archer1X, 386, 71, 100);
 		scorchedArena.add(p2ArcherLabel);
-		p2ArcherLabel.setBounds(archer2X, 338, 71, 100);
-		
+		p2ArcherLabel.setBounds(archer2X, 386, 71, 100);
+
 	}
 
 	public void importWall(){
@@ -217,22 +192,23 @@ public class ScorchedEarth {
 		} catch (IOException e) {
 			//do nothing
 		}
-		
+
 		p2ArcherLabel = new JLabel(new ImageIcon(archer2));
 		p2ArcherLabel.setPreferredSize(new Dimension (75, 100));
 		p1ArcherLabel = new JLabel(new ImageIcon(archer1));
 		p1ArcherLabel.setPreferredSize(new Dimension (75, 100));
-		
+
 	}
-	
+
 	public void archersX (){
 		archer1X = (int)(Math.random() * ((350) + 1));
 		archer2X =  650 + (int)(Math.random() * ((929 - 650) + 1));
-		
+
 	}
 
-	
+
 	public void setInfo() {
+
 		JLabel p1Name;
 		JLabel p2Name;
 		JLabel round;
@@ -240,7 +216,6 @@ public class ScorchedEarth {
 		JLabel p2Health;
 		JLabel p1HealthNum;
 		JLabel p2HealthNum;
-
 
 		p1Name = new JLabel("Player 1");
 		p1Name.setFont(new Font("Andalus", Font.BOLD, 40));
@@ -285,33 +260,99 @@ public class ScorchedEarth {
 
 	}
 
-	public void makeSelection() {
-
-
-	}
-
-	public class p1PowerListener implements ChangeListener {
+	public class powerListener implements ChangeListener {
 		public void stateChanged (ChangeEvent event) {
-			int power = p1PowerSlider.getValue();
+			int power = powerSlider.getValue();
 		}
 	}
 
-	public class p2PowerListener implements ChangeListener {
+	public class angleListener implements ChangeListener {
 		public void stateChanged (ChangeEvent event) {
-			int power = p2PowerSlider.getValue();
+			int angle = angleSlider.getValue();
 		}
 	}
 
-	public class p1AngleListener implements ChangeListener {
-		public void stateChanged (ChangeEvent event) {
-			int angle = p1AngleSlider.getValue();
+	public class getMoves{
+
+		getMoves(){
+			next.addMouseListener(new MouseInputAdapter(){
+
+				public void mousePressed(MouseEvent e){
+					if (click < 10){
+						click++;
+						if (click == 9){
+							next.setText("Play!");
+							turnIndicator.setText("P2 - Turn 5");
+						}
+						else if (click == 10){
+							turnIndicator.setText("Begin");
+						}
+						else if (click % 2 == 0){
+							turn++;
+							turnIndicator.setText("P1 - Turn " + turn);
+						}
+						else{
+							turnIndicator.setText("P2 - Turn " + turn);
+						}
+					}
+					System.out.println("click " + click);
+				}
+				public void mouseEntered(MouseEvent e){
+					next.setForeground(Color.red);
+				}
+				public void mouseExited(MouseEvent e){
+					next.setForeground(Color.white);
+				}
+
+			});
 		}
 	}
 
-	public class p2AngleListener implements ChangeListener {
-		public void stateChanged (ChangeEvent event) {
-			int angle = p2AngleSlider.getValue();
+	public class MouseListener{
+		
+		MouseListener(){
+			
+			next.addMouseListener(new MouseInputAdapter(){
+
+				public void mousePressed(MouseEvent e){
+					if (click < 10){
+						click++;
+						if (click == 9){
+							next.setText("Play!");
+							turnIndicator.setText("P2 - Turn 5");
+						}
+						else if (click == 10){
+							turnIndicator.setForeground(Color.black);
+						}
+						else if (click % 2 == 0){
+							turn++;
+							turnIndicator.setText("P1 - Turn " + turn);
+						}
+						else{
+							turnIndicator.setText("P2 - Turn " + turn);
+						}
+					}
+				}
+				public void mouseEntered(MouseEvent e){
+					next.setForeground(Color.red);
+				}
+				public void mouseExited(MouseEvent e){
+					next.setForeground(Color.white);
+				}
+
+			});
 		}
 	}
+	
+
+	public void playGame(){
+		
+
+	}
+
+
+
 
 }
+
+
--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -11,14 +11,12 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.WindowConstants;
-
 import java.awt.event.ComponentEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -45,6 +43,7 @@ public class TicTacToeGame implements MouseListener, KeyListener
 	private boolean theFlag;
 	private boolean won;
 	private boolean tie;
+	
 	//count wins
 	private int xWins;
 	private int oWins;
@@ -62,8 +61,7 @@ public class TicTacToeGame implements MouseListener, KeyListener
 	//constructor
 	public TicTacToeGame()
 	{
-
-
+		
 		int xWins = 0;
 		int oWins = 0;
 		won = false;
@@ -76,6 +74,7 @@ public class TicTacToeGame implements MouseListener, KeyListener
 		theJFrame.setSize(900, 600);
 		theJFrame.getBackground();
 		theJFrame.setBackground(Color.white);
+		theJFrame.setLocationRelativeTo(null);
 
 
 		theJFrame.setVisible(true);
